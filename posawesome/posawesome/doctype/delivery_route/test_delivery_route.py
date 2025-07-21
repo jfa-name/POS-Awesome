5d8b72c57b5cbe8179612cea72e76621bf73299b
Felipe Acosta <soporte@jfa.name>
2025-07-21 15:44:10 +0100

    Add Delivery Route doctype

---
+++ posawesome/posawesome/doctype/delivery_route/test_delivery_route.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Felipe Acosta and contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDeliveryRoute(FrappeTestCase):
+	pass
